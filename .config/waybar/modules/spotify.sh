fd5c3a981d40406b4c755454d82e8bdc3fd8fa69
nokkasiili12 <eliaslee04@gmail.com>
2026-04-28 22:40:35 +0300

    updated layout

--- .config/waybar/modules/spotify.sh
+++ .config/waybar/modules/spotify.sh
@@ -2,19 +2,19 @@
 
 class=$(playerctl metadata --player=spotify_player --format '{{status}}' 2>/dev/null | tr '[:upper:]' '[:lower:]')
 
-icon=""
+icon=" "
 text=""
 
 if [[ "$class" == "playing" ]]; then
-  info=$(playerctl metadata --player=spotify_player --format '{{artist}} - {{title}}' 2>/dev/null)
+    info=$(playerctl metadata --player=spotify_player --format '{{artist}} - {{title}}' 2>/dev/null)
 
-  if ((${#info} > 80)); then
-    info="${info:0:80}..."
-  fi
-  text=$info" "$icon
+    if ((${#info} > 120)); then
+        info="${info:0:120}..."
+    fi
+    text=$icon" "$info
 elif [[ $class == "paused" ]]; then
-  text=$icon
+    text=$icon
 elif [[ $class == "stopped" ]]; then
-  text=""
+    text=""
 fi
 echo -e "{\"text\":\""$text"\", \"class\":\""$class"\"}"
